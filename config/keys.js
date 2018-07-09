05ea736c2b00c4b00184c62f65a7abb978cc9891
zeshanchoudhary <zeshan@doubleok.com>
2018-07-09 19:01:56 +0200

    InInitial commit and Finished lecture number 5

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: 'mongodb://zeshan:zeshan123@ds231941.mlab.com:31941/mern-traversy'
+};
---
+++ routes/api/items.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+const Item = require('../../models/Item');
+
+// @route GET api/items
+router.get('/', (req, res) => {
+  return Item.find({})
+    .sort({ date: -1 })
+    .then(items => res.json(items));
+});
+
+// @route POST api/items
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
