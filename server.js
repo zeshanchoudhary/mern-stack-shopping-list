a7f880e27a98531e696471f34b38570652ed294c
zeshanchoudhary <zeshan@doubleok.com>
2018-07-10 16:41:53 +0200

    Ready to commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -17,6 +17,16 @@ mongoose
 
 // Use routes
 app.use('/api/items', items);
+
+// Server Static assets if in productions
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (re, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
